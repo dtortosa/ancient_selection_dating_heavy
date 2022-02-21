97427dd2eaf39f049a9943aa3a1673713320f69b
dtortosa <dftortosa@gmail.com>
2022-02-21 14:04:36 -0700

    run again the container with 100 chunks in 8 random genomes

--- sweep_enrichments/david_pipeline/exdef_folder/bootstrap_rafale.sh
+++ sweep_enrichments/david_pipeline/exdef_folder/bootstrap_rafale.sh
@@ -1,2 +1,11 @@
-./bootstrap_test_script.pl ./ valid_file.txt all_ranks_ihs_1000kb ensembl_hgnc_file.txt genes_set_file.txt factors_table.txt distance_file.txt 100 0.5 ./VIPs_parts/file_1 ./nonVIPs_parts/file_1 300000 no 10
+./bootstrap_test_script.pl ./ valid_file.txt all_ranks_ihs_50kb ensembl_hgnc_file.txt genes_set_file.txt factors_table.txt distance_file.txt 1000 0.5 ./VIPs_parts/file_1 ./nonVIPs_parts/file_1 300000 no 10 & 
+./bootstrap_test_script.pl ./ valid_file.txt all_ranks_ihs_50kb ensembl_hgnc_file.txt genes_set_file.txt factors_table.txt distance_file.txt 1000 0.5 ./VIPs_parts/file_2 ./nonVIPs_parts/file_2 300000 no 10 & 
+./bootstrap_test_script.pl ./ valid_file.txt all_ranks_ihs_50kb ensembl_hgnc_file.txt genes_set_file.txt factors_table.txt distance_file.txt 1000 0.5 ./VIPs_parts/file_3 ./nonVIPs_parts/file_3 300000 no 10 & 
+./bootstrap_test_script.pl ./ valid_file.txt all_ranks_ihs_50kb ensembl_hgnc_file.txt genes_set_file.txt factors_table.txt distance_file.txt 1000 0.5 ./VIPs_parts/file_4 ./nonVIPs_parts/file_4 300000 no 10 & 
+./bootstrap_test_script.pl ./ valid_file.txt all_ranks_ihs_50kb ensembl_hgnc_file.txt genes_set_file.txt factors_table.txt distance_file.txt 1000 0.5 ./VIPs_parts/file_5 ./nonVIPs_parts/file_5 300000 no 10 & 
+./bootstrap_test_script.pl ./ valid_file.txt all_ranks_ihs_50kb ensembl_hgnc_file.txt genes_set_file.txt factors_table.txt distance_file.txt 1000 0.5 ./VIPs_parts/file_6 ./nonVIPs_parts/file_6 300000 no 10 & 
+./bootstrap_test_script.pl ./ valid_file.txt all_ranks_ihs_50kb ensembl_hgnc_file.txt genes_set_file.txt factors_table.txt distance_file.txt 1000 0.5 ./VIPs_parts/file_7 ./nonVIPs_parts/file_7 300000 no 10 & 
+./bootstrap_test_script.pl ./ valid_file.txt all_ranks_ihs_50kb ensembl_hgnc_file.txt genes_set_file.txt factors_table.txt distance_file.txt 1000 0.5 ./VIPs_parts/file_8 ./nonVIPs_parts/file_8 300000 no 10 & 
+./bootstrap_test_script.pl ./ valid_file.txt all_ranks_ihs_50kb ensembl_hgnc_file.txt genes_set_file.txt factors_table.txt distance_file.txt 1000 0.5 ./VIPs_parts/file_9 ./nonVIPs_parts/file_9 300000 no 10 & 
+./bootstrap_test_script.pl ./ valid_file.txt all_ranks_ihs_50kb ensembl_hgnc_file.txt genes_set_file.txt factors_table.txt distance_file.txt 1000 0.5 ./VIPs_parts/file_10 ./nonVIPs_parts/file_10 300000 no 10
 sleep 5
