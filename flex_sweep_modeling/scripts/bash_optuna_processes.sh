78b84b209c1a4bedd53d0b7cfb24739fc999bbc5
dtortosa <dftortosa@gmail.com>
2023-01-26 20:15:53 -0700

    preparing a new run of optuna

--- flex_sweep_modeling/scripts/bash_optuna_processes.sh
+++ flex_sweep_modeling/scripts/bash_optuna_processes.sh
@@ -11,12 +11,17 @@
 
 #run several scripts each one runnning an optuna process with different seeds but all using the same relational database
 #seeds changed in the second run
-python3.9 /opt/scripts/flex_sweep_continuous_modeling_yoruba.py --optuna_seed=8765 --n_trials=20000 &
+python3.9 /opt/scripts/flex_sweep_continuous_modeling_yoruba.py --optuna_seed=0432 --n_trials=20000 &
+#--optuna_seed=8765
 sleep 30s
 	#we need delay the next process in order to leave time the first one to create
 	#the database and avoid errors because two processes are creating the same db
 	#https://stackoverflow.com/questions/49944364/how-to-run-two-commands-but-with-a-delay-on-the-second-command-without-stopping
-python3.9 /opt/scripts/flex_sweep_continuous_modeling_yoruba.py --optuna_seed=4572 --n_trials=20000 &
-python3.9 /opt/scripts/flex_sweep_continuous_modeling_yoruba.py --optuna_seed=5234 --n_trials=20000 &
-python3.9 /opt/scripts/flex_sweep_continuous_modeling_yoruba.py --optuna_seed=0976 --n_trials=20000 &
-python3.9 /opt/scripts/flex_sweep_continuous_modeling_yoruba.py --optuna_seed=3413 --n_trials=20000
+python3.9 /opt/scripts/flex_sweep_continuous_modeling_yoruba.py --optuna_seed=9456 --n_trials=20000 &
+#--optuna_seed=4572
+python3.9 /opt/scripts/flex_sweep_continuous_modeling_yoruba.py --optuna_seed=0467 --n_trials=20000 &
+#--optuna_seed=5234
+python3.9 /opt/scripts/flex_sweep_continuous_modeling_yoruba.py --optuna_seed=1624 --n_trials=20000 &
+#--optuna_seed=0976
+python3.9 /opt/scripts/flex_sweep_continuous_modeling_yoruba.py --optuna_seed=8563 --n_trials=20000
+#--optuna_seed=3413
